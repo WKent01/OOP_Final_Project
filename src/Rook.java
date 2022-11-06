717553b77fee82fd5cac4aad5a805ff9ebb31017
WKent01 <kentwoolner@gmail.com>
2022-11-06 17:31:42 -0600

    Added Rook validMoves method

--- src/Rook.java
+++ src/Rook.java
@@ -8,6 +8,61 @@ public class Rook extends Piece {
 
     @Override
     public ArrayList<Square> getValidMoves() {
-        return null;
+
+        ArrayList<Square> moves = new ArrayList<>();
+
+        try {
+            for (int x = this.pos_X; x < 8; x++) {
+                if (ChessBoard.squares[x][this.pos_Y].isOccupied()
+                        && !ChessBoard.squares[x][this.pos_Y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[x][this.pos_Y]);
+                    break;
+                } else if (ChessBoard.squares[x][this.pos_Y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[x][this.pos_Y]);
+                }
+            }
+
+            for (int x = this.pos_X; x >= 0; x--) {
+                if (ChessBoard.squares[x][this.pos_Y].isOccupied()
+                        && !ChessBoard.squares[x][this.pos_Y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[x][this.pos_Y]);
+                    break;
+                } else if (ChessBoard.squares[x][this.pos_Y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[x][this.pos_Y]);
+                }
+            }
+
+            for (int y = this.pos_Y; y < 8; y++) {
+                if (ChessBoard.squares[this.pos_X][y].isOccupied()
+                        && !ChessBoard.squares[this.pos_X][y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[this.pos_X][y]);
+                    break;
+                } else if (ChessBoard.squares[this.pos_X][y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[this.pos_X][y]);
+                }
+            }
+
+            for (int y = this.pos_Y; y >= 0; y--) {
+                if (ChessBoard.squares[this.pos_X][y].isOccupied()
+                        && !ChessBoard.squares[this.pos_X][y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[this.pos_X][y]);
+                    break;
+                } else if (ChessBoard.squares[this.pos_X][y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[this.pos_X][y]);
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+
+        return moves;
     }
 }
