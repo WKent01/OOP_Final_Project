75426c87b11c5a42eca8f74992cdde6ccb9d883c
WKent01 <kentwoolner@gmail.com>
2022-11-06 17:39:10 -0600

    Created the validMoves for Bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -9,6 +9,61 @@ public class Bishop extends Piece {
 
     @Override
     public ArrayList<Square> getValidMoves() {
-        return null;
+
+        ArrayList<Square> moves = new ArrayList<>();
+
+        try {
+            for (int x = this.pos_X, y = this.pos_Y; x < 8 && y < 8; x++, y++) { // Checks the SE diagonal
+                if (ChessBoard.squares[x][y].isOccupied()
+                        && !ChessBoard.squares[x][y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[x][y]);
+                    break;
+                } else if (ChessBoard.squares[x][y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[x][y]);
+                }
+            }
+
+            for (int x = this.pos_X, y = this.pos_Y; x < 8 && y >= 0; x++, y--) { // Checks the NE diagonal
+                if (ChessBoard.squares[x][y].isOccupied()
+                        && !ChessBoard.squares[x][y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[x][y]);
+                    break;
+                } else if (ChessBoard.squares[x][y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[x][y]);
+                }
+            }
+
+            for (int x = this.pos_X, y = this.pos_Y; x >= 0 && y >= 0; x--, y--) { // Checks the NW diagonal
+                if (ChessBoard.squares[x][y].isOccupied()
+                        && !ChessBoard.squares[x][y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[x][y]);
+                    break;
+                } else if (ChessBoard.squares[x][y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[x][y]);
+                }
+            }
+
+            for (int x = this.pos_X, y = this.pos_Y; x >= 0 && y < 8; x--, y++) { // Checks the SW diagonal
+                if (ChessBoard.squares[x][y].isOccupied()
+                        && !ChessBoard.squares[x][y].pieceOnSquare().getColor().equals(this.color)) {
+                    moves.add(ChessBoard.squares[x][y]);
+                    break;
+                } else if (ChessBoard.squares[x][y].isOccupied()) {
+                    continue;
+                } else {
+                    moves.add(ChessBoard.squares[x][y]);
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+
+        return moves;
     }
 }
